31390fcf3ef629c50cdf0f804ce57d3e29e66a57
Bobur Otaboyev <boburotaboyev0@gmail.com>
2025-04-24 15:53:59 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_web_rtc_example
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/uz/assoft/flutter_web_rtc_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package uz.assoft.flutter_web_rtc_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
